9586ee0f039a8bbe59d9f2f5fde6dffbc06aa2c5
shayan <shayanadc@gmail.com>
2021-02-20 13:21:09 +0330

    add test case

--- tests/tests/BinaryConvertorTest.php
+++ tests/tests/BinaryConvertorTest.php
@@ -3,12 +3,23 @@ use PHPUnit\Framework\TestCase;
 
 class BinaryConvertorTest extends TestCase
 {
-    public function testConvertBinaryToWord()
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testConvertBinaryToWord($input,$expected)
     {
         $calculator = new \App\Utils\BinaryConvertor();
-        $result = $calculator->makeWordFrom('01000011 01100101 01101100 01101100');
+        $result = $calculator->makeWordFrom($input);
 
-        // assert that your calculator added the numbers correctly!
-        $this->assertEquals('Cell', $result);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function additionProvider(): array
+    {
+        return [
+            ['01000011 01100101 01101100 01101100', 'Cell'],
+            ['01000100 01100101 01110100 01100101 01101110 01110100 01101001 01101111 01101110', 'Detention'],
+            ['00100000 00110010 00110001 00111000 00110111', ' 2187'],
+        ];
     }
 }
